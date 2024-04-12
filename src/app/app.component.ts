cd5aa293cfed66612e7b0e9a944a4a000eca8273
Cristian Rodriguez <cristian.rodriguez@makingsense.com>
2024-04-12 00:50:23 -0300

    36. Formulario para Agregar Nueva Persona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Persona } from './persona.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'Listado de Personas';
+  personas: Persona[] = [
+    new Persona('Cristian', 'Rodriguez'),
+    new Persona('Micaela', 'Rodriguez'),
+    new Persona('Melina', 'Rodriguez'),
+  ];
+
+  nombreInput: string = '';
+  apellidoInput: string = '';
+
+  agregarPersona() {
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona1);
+  }
 }
